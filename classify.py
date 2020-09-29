b6ea0fc626fd73ac760f0e1236c258c70e106002
shaelsien630 <shaelsien630@gmail.com>
2020-09-29 15:39:16 +0900

    make it to class

--- classify.py
+++ classify.py
@@ -12,72 +12,84 @@ import pickle
 import cv2
 import os
 
-def setGPU():
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
+class Classification:
+    def __init__(self):
+        self.result = ""
 
-def imagePreprocessing(imagePath):
-    # load the image
-    image = cv2.imread(imagePath)
-    originalImage = image.copy()
+    @staticmethod
+    def setGPU():
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                # Currently, memory growth needs to be the same across GPUs
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                # Memory growth must be set before GPUs have been initialized
+                print(e)
 
-    # pre-process the image for classification
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # gray scale (모델 train 시 전처리에 사용) 추가함
-    image = cv2.resize(image, (96, 96))
-    image = image.astype("float") / 255.0
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    return image, originalImage
+    @staticmethod
+    def imagePreprocessing(imagePath):
+        # load the image
+        image = cv2.imread(imagePath)
+        originalImage = image.copy()
 
-def loadModel(modelPath, labelbinPath):
-    # load the trained convolutional neural network and the label
-    # binarizer
-    print("[INFO] loading network...")
-    model = load_model(modelPath)
-    lb = pickle.loads(open(labelbinPath, "rb").read())
-    return model, lb
+        # pre-process the image for classification
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # gray scale (모델 train 시 전처리에 사용) 추가함
+        image = cv2.resize(image, (96, 96))
+        image = image.astype("float") / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        return image, originalImage
 
-def printAllLabels(lb, proba):
-    # 라벨 전체 출력
-    for i in range(0, len(lb.classes_)):
-        lb_test = "{}: {:.2f}% ".format(lb.classes_[i], proba[i] * 100)
-        print(lb_test)
+    @staticmethod
+    def loadModel(modelPath, labelbinPath):
+        # load the trained convolutional neural network and the label
+        # binarizer
+        print("[INFO] loading network...")
+        model = load_model(modelPath)
+        lb = pickle.loads(open(labelbinPath, "rb").read())
+        return model, lb
 
-def classify(image, model, lb, output = []):
-    # classify the input image
-    print("[INFO] classifying image...")
-    proba = model.predict(image)[0]
-    idx = np.argmax(proba)
-    label = lb.classes_[idx]
+    @staticmethod
+    def printAllLabels(lb, proba):
+        # 라벨 전체 출력
+        for i in range(0, len(lb.classes_)):
+            lb_test = "{}: {:.2f}% ".format(lb.classes_[i], proba[i] * 100)
+            print(lb_test)
 
-    # printAllLabels(lb, proba)
+    @staticmethod
+    def classify(image, model, lb, output = [], showResult = False):
+        # classify the input image
+        print("[INFO] classifying image...")
+        proba = model.predict(image)[0]
+        idx = np.argmax(proba)
+        label = lb.classes_[idx]
+        result = label
+        # printAllLabels(lb, proba)
 
-    # we'll mark our prediction as "correct" of the input image filename
-    # contains the predicted label text (obviously this makes the
-    # assumption that you have named your testing image files this way)
-    filename = args["image"][args["image"].rfind(os.path.sep) + 1:]
-    correct = "correct" if filename.rfind(label) != -1 else "incorrect"
+        if showResult:
+            # we'll mark our prediction as "correct" of the input image filename
+            # contains the predicted label text (obviously this makes the
+            # assumption that you have named your testing image files this way)
+            filename = args["image"][args["image"].rfind(os.path.sep) + 1:]
+            correct = "correct" if filename.rfind(label) != -1 else "incorrect"
 
-    # build the label and draw the label on the image
-    label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
-    # show the output image
-    print("[INFO] {}".format(label))
+            # build the label and draw the label on the image
+            label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
+            # show the output image
+            print("[INFO] {}".format(label))
 
-    if len(output) != 0:
-        output = imutils.resize(output, width=400)
-        cv2.putText(output, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (0, 255, 0), 2)
-        cv2.imshow("Output", output)
-        cv2.waitKey(0)
+            if len(output) != 0:
+                output = imutils.resize(output, width=400)
+                cv2.putText(output, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7, (0, 255, 0), 2)
+                cv2.imshow("Output", output)
+                cv2.waitKey(0)
+
+        return result
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -89,8 +101,10 @@ ap.add_argument("-i", "--image", required=True,
                 help="path to input image")
 args = vars(ap.parse_args())
 
-setGPU()
-image, originalImage = imagePreprocessing(args["image"])
-model, lb = loadModel(args["model"], args["labelbin"])
+Classification.setGPU()
+model, lb = Classification.loadModel(args["model"], args["labelbin"])
 
-classify(image, model, lb, output=originalImage)
+image, originalImage = Classification.imagePreprocessing(args["image"])
+result = Classification.classify(image, model, lb)
+print(result)
+
